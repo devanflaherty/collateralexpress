e6cb81b353c767ee531ffc9401aa421172a31478
Devan Flaherty <devanflaherty@me.com>
2017-06-07 12:14:27 -0600

    Playing with the react implementation in Rails
    - Simply testing React in rails

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,19 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
-console.log('Hello World from Webpacker')
+import { Header, Footer } from './components/Layout'
+
+const App = () =>
+    <div>
+      <Header name="react" />
+      <Footer name="foot" />
+    </div>
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <App />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
