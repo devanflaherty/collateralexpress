19482534f68baf50f588add05d586f34a1fc1e8c
Devan Flaherty <devanflaherty@me.com>
2017-07-13 17:26:18 -0600

    Static pages initial design (WIP)

---
+++ app/webpack/packs/swiper.js
@@ -0,0 +1,32 @@
+import Swiper from "swiper/dist/js/swiper.js"
+
+document.addEventListener("turbolinks:load", function() {
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    effect: 'fade',
+    speed: 1000,
+    fade: {
+      crossFade: false
+    },
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    slidesPerView: 1,
+    paginationClickable: true,
+    parallax:true
+  })
+
+  var active = Number
+  active = mySwiper.activeIndex + 1
+  $("[data-key="+ active +"]").find($(".step-desc")).addClass('fade-in')
+
+  mySwiper.on("onSlideChangeStart",(mySwiper) => {
+    active = mySwiper.activeIndex + 1
+    $("#stepIndex").text(active)
+    $(".step-desc").removeClass('fade-in')
+  });
+
+  mySwiper.on("onSlideChangeEnd",(mySwiper) => {
+    $("[data-key="+ active +"]").find($(".step-desc")).addClass('fade-in')
+  });
+})
