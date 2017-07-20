913000846e829003a6b294b96a797e7c51922061
Devan Flaherty <devanflaherty@me.com>
2017-07-20 04:08:14 -0600

    Remove es6 code from swiper js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,13 +40,13 @@ document.addEventListener("turbolinks:load", function() {
     active = mySwiper.activeIndex + 1
     $("[data-key="+ active +"]").find($(".step-desc")).addClass('fade-in')
 
-    mySwiper.on("onSlideChangeStart",(mySwiper) => {
+    mySwiper.on("onSlideChangeStart",(function (mySwiper) {
       active = mySwiper.activeIndex + 1
       $("#stepIndex").text(active)
       $(".step-desc").removeClass('fade-in')
     });
 
-    mySwiper.on("onSlideChangeEnd",(mySwiper) => {
+    mySwiper.on("onSlideChangeEnd",(function (mySwiper) {
       $("[data-key="+ active +"]").find($(".step-desc")).addClass('fade-in')
     });
   }
