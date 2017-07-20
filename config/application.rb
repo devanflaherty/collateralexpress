2f932d8d57408c45b4570e9ad3b4d5b1b2f9b484
Devan Flaherty <devanflaherty@me.com>
2017-07-20 10:29:50 -0600

    Static asset fixes

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.serve_static_assets = true
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -12,6 +10,9 @@ module CollateralExpress
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    config.serve_static_assets = true
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
